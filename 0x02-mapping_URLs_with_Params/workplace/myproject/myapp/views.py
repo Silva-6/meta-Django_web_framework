6d594f76e109118adb3a27648102e7189d303d7e
Silva-6 <silva6media@gmail.com>
2022-12-21 08:25:55 +0100

    modify views and urls

--- 0x02-mapping_URLs_with_Params/workplace/myproject/myapp/views.py
+++ 0x02-mapping_URLs_with_Params/workplace/myproject/myapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def drink(request, drink_name):
+    drinks = {'mocha': 'type of coffee', 'tea': 'type of beverages', 'lemonade': 'type of refreshment', 'choice_of_drink': drink_name}
+    return HttpResponse(f"<h2> {drinks} </h2>")
