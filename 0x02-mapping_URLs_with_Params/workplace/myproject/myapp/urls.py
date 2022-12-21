7f61760ded99a329efa9495821a084ade2b05824
Silva-6 <silva6media@gmail.com>
2022-12-21 19:45:47 +0100

    make an adjustment

--- 0x02-mapping_URLs_with_Params/workplace/myproject/myapp/urls.py
+++ 0x02-mapping_URLs_with_Params/workplace/myproject/myapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('drinks/<str:drink_name>', views.drinks, name='drinks')
+    path('drinks/<str:drink_name>', views.drinks, name="drinks"),
 ]
--- 0x02-mapping_URLs_with_Params/workplace/myproject/myapp/views.py
+++ 0x02-mapping_URLs_with_Params/workplace/myproject/myapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def drink(request, drink_name):
-    drinks = {'mocha': 'type of coffee', 'tea': 'type of beverages', 'lemonade': 'type of refreshment', 'choice_of_drink': drink_name}
-    return HttpResponse(f"<h2> {drinks} </h2>")
+def drinks(request, drink_name):
+    drink = {'mocha': 'type of coffee', 'tea': 'type of beverages', 'lemonade': 'type of refreshment'}
+    choice_of_drink = drink.drink_name
+    return HttpResponse(f"<h2> {drink_name} </h2><p> {choice_of_drink} </p>")
