a37677ef1ec706b4b2078fa12fdc4226976d6bcc
Silva-6 <silva6media@gmail.com>
2022-12-21 22:33:40 +0100

    update

--- 0x02-mapping_URLs_with_Params/workplace/myproject/myapp/views.py
+++ 0x02-mapping_URLs_with_Params/workplace/myproject/myapp/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def drinks(request, drink_name):
-    drink = {'mocha': 'type of coffee', 'tea': 'type of beverages', 'lemonade': 'type of refreshment'}
-    choice_of_drink = drink.drink_name
-    return HttpResponse(f"<h2> {drink_name} </h2><p> {choice_of_drink} </p>")
+    drink = {
+        'mocha': 'type of coffee', 
+        'tea': 'type of beverages', 
+        'lemonade': 'type of refreshment'
+    }
+    choice_of_drink = drink[drink_name]
+    return HttpResponse(f"<h2> {drink_name} </h2>" + choice_of_drink)
