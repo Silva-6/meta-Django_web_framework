77c1800dba311d2692fdc2a5bbc1417fa72968cf
Silva-6 <silva6media@gmail.com>
2023-01-07 16:00:30 +0100

    updated

--- 0x06-models_using_foreign_keys/workplace/myproject/myapp/admin.py
+++ 0x06-models_using_foreign_keys/workplace/myproject/myapp/admin.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 from .models import DrinksCategory, Drinks
 
 # Register your models here.
-admin.site.register(DrinksCategory, Drinks)
+admin.site.register(DrinksCategory)
+admin.site.register(Drinks)
