872586862e1102823fcf895d44b536c6cf09e7e5
Silva-6 <silva6media@gmail.com>
2023-01-07 21:55:04 +0100

    updated

--- 0x07-using_Django_admin/workplace/myproject/myapp/models.py
+++ 0x07-using_Django_admin/workplace/myproject/myapp/models.py
@@ -6,3 +6,7 @@ class Employees(models.Model):
     last_name = models.CharField(max_length=200)
     role = models.CharField(max_length=100)
     shift = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.first_name
+        
