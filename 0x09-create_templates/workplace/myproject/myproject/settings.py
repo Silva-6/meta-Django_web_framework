9956bf73cb62834d66bbc55c1c9c8c28a7f66800
Silva-6 <silva6media@gmail.com>
2023-01-09 07:54:10 +0100

    updated

--- 0x09-create_templates/workplace/myproject/myproject/settings.py
+++ 0x09-create_templates/workplace/myproject/myproject/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = ['myapp/static',]
+
 # Update the list variable for accesssing app-level static files
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
